898aa8b9bb1c085c455098d93bc2a0153a1378df
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-15 19:33:58 -0300

    Create CloudFormation Stacks for Vpc, Cluster and Service01

--- curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
+++ curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -8,7 +8,13 @@ public class CursoAwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc");
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
 
         app.synth();
     }
