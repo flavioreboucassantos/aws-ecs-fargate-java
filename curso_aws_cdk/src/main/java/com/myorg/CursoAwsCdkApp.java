c8bfae7a2bf89073f468337d1d2f9d7a4bda1aae
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-23 19:17:44 -0300

    Create Invoice Stack

--- curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
+++ curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -15,12 +15,16 @@ public class CursoAwsCdkApp {
 		rdsStack.addDependency(vpcStack);
 
 		SnsStack snsStack = new SnsStack(app, "Sns");
+		
+		InvoiceAppStack invoiceAppStack = new InvoiceAppStack(app, "InvoiceApp");
 
 		Service01Stack service01Stack = new Service01Stack(app, "Service01",
-				clusterStack.getCluster(), snsStack.getProductEventsTopic());
+				clusterStack.getCluster(), snsStack.getProductEventsTopic(),
+				invoiceAppStack.getBucket(), invoiceAppStack.getS3InvoiceQueue());
 		service01Stack.addDependency(clusterStack);
 		service01Stack.addDependency(rdsStack);
 		service01Stack.addDependency(snsStack);
+		service01Stack.addDependency(invoiceAppStack);
 
 		DdbStack ddbStack = new DdbStack(app, "Ddb");
 		
--- curso_aws_cdk/src/main/java/com/myorg/Service01Stack.java
+++ curso_aws_cdk/src/main/java/com/myorg/Service01Stack.java
@@ -1,23 +1,37 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Duration;
+import software.amazon.awscdk.core.Fn;
+import software.amazon.awscdk.core.RemovalPolicy;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
-import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.ScalableTaskCount;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
-
-import java.util.HashMap;
-import java.util.Map;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.sqs.Queue;
 
 public class Service01Stack extends Stack {
-	public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic) {
-		this(scope, id, null, cluster, productEventsTopic);
+	public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic,
+			Bucket invoiceBucket, Queue invoiceQueue) {
+		this(scope, id, null, cluster, productEventsTopic, invoiceBucket, invoiceQueue);
 	}
 
-	public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productEventsTopic) {
+	public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster,
+			SnsTopic productEventsTopic, Bucket invoiceBucket, Queue invoiceQueue) {
 		super(scope, id, props);
 
 		Map<String, String> envVariables = new HashMap<>();
@@ -26,10 +40,15 @@ public class Service01Stack extends Stack {
 				+ ":3306/aws_project01?createDatabaseIfNotExist=true");
 		envVariables.put("SPRING_DATASOURCE_USERNAME", "admin");
 		envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
+		
 		envVariables.put("AWS_REGION", "sa-east-1");
 		envVariables.put("AWS_SNS_TOPIC_PRODUCT_EVENTS_ARN", productEventsTopic.getTopic().getTopicArn());
+		
+		envVariables.put("AWS_S3_BUCKET_INVOICE_NAME", invoiceBucket.getBucketName());		
+        envVariables.put("AWS_SQS_QUEUE_INVOICE_EVENTS_NAME", invoiceQueue.getQueueName());
 
-		ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
+		ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
+				.create(this, "ALB01")
 				.serviceName("service-01")
 				.cluster(cluster)
 				.cpu(256)
@@ -39,7 +58,8 @@ public class Service01Stack extends Stack {
 				.taskImageOptions(
 						ApplicationLoadBalancedTaskImageOptions.builder()
 								.containerName("aws_project01")
-								.image(ContainerImage.fromRegistry("flavioreboucassantos/curso_aws-ecs-fargate-java_aws_project01:1.6.0"))
+								.image(ContainerImage.fromRegistry(
+										"flavioreboucassantos/curso_aws-ecs-fargate-java_aws_project01:1.7.0"))
 								.containerPort(8080)
 								.logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
 										.logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
@@ -71,5 +91,7 @@ public class Service01Stack extends Stack {
 				.build());
 
 		productEventsTopic.getTopic().grantPublish(service01.getTaskDefinition().getTaskRole());
+		invoiceQueue.grantConsumeMessages(service01.getTaskDefinition().getTaskRole());
+        invoiceBucket.grantReadWrite(service01.getTaskDefinition().getTaskRole());
 	}
 }
