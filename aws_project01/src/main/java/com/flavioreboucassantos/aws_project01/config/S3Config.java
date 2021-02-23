b48628ba7ed4e3956e44670a86d27a808c41779f
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-23 19:21:39 -0300

    Create Invoice (S3 -> SQS) Consumer & Save, Controller Url & Find

---
+++ aws_project01/src/main/java/com/flavioreboucassantos/aws_project01/config/S3Config.java
@@ -0,0 +1,23 @@
+package com.flavioreboucassantos.aws_project01.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Config {
+
+	@Value("${aws.region}")
+	private String awsRegion;
+
+	@Bean
+	public AmazonS3 amazonS3Client() {
+		return AmazonS3ClientBuilder.standard()
+				.withRegion(awsRegion)
+				.withCredentials(new DefaultAWSCredentialsProviderChain())
+				.build();
+	}
+
+}
